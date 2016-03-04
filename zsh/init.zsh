f121312f9e6073d61b8a212b60042d80e2bfb55d
Mark Glenn <markglenn@gmail.com>
2016-03-04 21:47:12 +0000

    Add plugins

--- zsh/init.zsh
+++ zsh/init.zsh
@@ -1,9 +1,35 @@
-for lib_file ($HOME/.code/zsh/lib/*.zsh); do
+for lib_file ($ZSH/lib/*.zsh); do
   source $lib_file
 done
 
-for plugin_file ($HOME/.code/zsh/plugins/*.zsh); do
-  source $plugin_file
+is_plugin() {
+  local base_dir=$1
+  local name=$2
+  test -f $base_dir/plugins/$name/$name.plugin.zsh \
+    || test -f $base_dir/plugins/$name/_$name
+}
+# Add all defined plugins to fpath. This must be done
+# before running compinit.
+for plugin ($plugins); do
+  if is_plugin $ZSH $plugin; then
+    fpath=($ZSH/plugins/$plugin $fpath)
+  fi
+done
+
+# Save the location of the current completion dump file.
+if [ -z "$ZSH_COMPDUMP" ]; then
+  ZSH_COMPDUMP="${ZDOTDIR:-${HOME}}/.zcompdump-${SHORT_HOST}-${ZSH_VERSION}"
+fi
+
+# Load and run compinit
+autoload -U compinit
+compinit -i -d "${ZSH_COMPDUMP}"
+
+# Load all of the plugins that were defined in ~/.zshrc
+for plugin ($plugins); do
+  if [ -f $ZSH/plugins/$plugin/$plugin.plugin.zsh ]; then
+    source $ZSH/plugins/$plugin/$plugin.plugin.zsh
+  fi
 done
 
 source $HOME/.code/zsh/themes/basic.zsh-theme
